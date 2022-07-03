4fe295a9ea757ed4d61a7dc46bb7801ab7bb651d
AsmaaAamir <asma.liaqat@outlook.com>
2022-07-03 20:43:27 +0000

    add fucntion to js

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+    
+}
+
+function calculateCorrectAnswer(){
+    
+}
+
+function incrementScore(){
+    
+}
+
+function incrementWrongAnswer (){
+    
+}
+
+function displayAdditionQuestion (){
+    
+}
+
+function displaySubratQuestion (){
+    
+}
+
+function displayMulitplyQuestion (){
+    
+}
+
+function displayDivisionQuestion (){
+    
+}
