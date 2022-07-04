2e45907967e3f4c9241513ca1d3411018ae6c155
AsmaaAamir <asma.liaqat@outlook.com>
2022-07-04 23:08:14 +0000

    add division code

--- assets/js/script.js
+++ assets/js/script.js
@@ -83,7 +83,7 @@ function calculateCorrectAnswer() {
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
     } else if (operator === "/") {
-        return [operand1 - operand2, "division"];
+        return [operand1 / operand2, "division"];
     } 
     else {
         alert(`Unimplemented operator ${operator}`);
